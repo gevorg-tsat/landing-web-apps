2055f49d0e3e4109939b38c20d591ac47dbde8d1
gevorg-tsat <72250109+gevorg-tsat@users.noreply.github.com>
2023-10-06 18:51:44 +0300

    how does it work page: in progress

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,14 +1,14 @@
 import React from "react";
-import PickMeals from "../Assets/pick-meals-image.png";
+import Neuronet from "../Assets/neuronet-image.png";
 import ChooseMeals from "../Assets/choose-image.png";
 import DeliveryMeals from "../Assets/delivery-image.png";
 
 const Work = () => {
   const workInfoData = [
     {
-      image: PickMeals,
-      title: "Pick Meals",
-      text: "Lorem ipsum dolor sit amet consectetur. Maecenas orci et sagittis duis elementum interdum facilisi bibendum.",
+      image: Neuronet,
+      title: "Нейросеть",
+      text: "Будет разработана модель для определения состояния света в кабинете(вкл/выкл)",
     },
     {
       image: ChooseMeals,
@@ -24,12 +24,12 @@ const Work = () => {
   return (
     <div className="work-section-wrapper">
       <div className="work-section-top">
-        <p className="primary-subheading">Work</p>
-        <h1 className="primary-heading">How It Works</h1>
-        <p className="primary-text">
+        {/* <p className="primary-subheading"></p> */}
+        <h1 className="primary-heading">Как это работает?</h1>
+        {/* <p className="primary-text">
           Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
           elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
-        </p>
+        </p> */}
       </div>
       <div className="work-section-bottom">
         {workInfoData.map((data) => (
